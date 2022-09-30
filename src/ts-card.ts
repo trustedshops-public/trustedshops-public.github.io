f5af69bec1c5f27fdf0134d8d0d958b519da5ca0
Wiktoria Mielcarek <wiktoria.mielcarek@trustedshops.com>
2022-09-30 14:12:30 +0200

    create repositroy card component

---
+++ src/ts-card.ts
@@ -0,0 +1,35 @@
+import { LitElement, css, html } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
+import { Repository } from './ts-main';
+
+@customElement('ts-card')
+export class TsCard extends LitElement {
+  @property()
+  repository: string = '';
+  @state()
+  repo: Repository = {
+    id: -1,
+    name: '',
+    html_url: '',
+    description: '',
+    created_at: '',
+    updated_at: '',
+    topics: [],
+  };
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    this.repo = JSON.parse(this.repository);
+  }
+
+  render() {
+    console.log(this.repo);
+    return html`<div>
+      <h4>${this.repo.name}</h4>
+      <p>${this.repo.description}</p>
+      <ul>
+        ${this.repo.topics.map((topic) => html`<li>${topic}</li>`)}
+      </ul>
+    </div>`;
+  }
+}
