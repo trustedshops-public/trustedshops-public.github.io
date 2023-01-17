0d4bf89809d509fe63e6ad2f012b91dc16016fea
Wiktoria Mielcarek <wiktoria.mielcarek@trustedshops.com>
2023-01-17 16:31:03 +0100

    add privacy page

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "build",
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy/index.html'),
+      },
+    },
   },
 });
