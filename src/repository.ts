a128ee3b3874a9e2675c7a6c8374159c8f208d36
Timo Reymann <Timo.Reymann@trustedshops.de>
2023-01-18 10:21:05 +0100

    chore: Move repository code into shared file

---
+++ src/repository.ts
@@ -0,0 +1,36 @@
+export const githubApi =
+  'https://api.github.com/users/trustedshops-public/repos?per_page=100&type=owner';
+
+export type GroupRepository = Map<string, Repository[]>;
+
+export type Repository = {
+  id: number;
+  name: string;
+  html_url: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+  topics: string[];
+  [key: string]: unknown;
+};
+
+export const getOrderedRepositories = (list: Repository[]): GroupRepository => {
+  const data: GroupRepository = new Map();
+
+  list.forEach((item) => {
+    const [topic] = item.topics.filter((topic: string) =>
+      topic.startsWith('ts')
+    );
+
+    if (!topic) {
+      return;
+    }
+
+    const title = topic.substring(2);
+
+    data.has(title)
+      ? data.set(title, [...data.get(title)!, item])
+      : data.set(title, [item]);
+  });
+  return data;
+};
